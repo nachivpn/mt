acd2dc2d5b1da43511a46a217cee73fe66a501e9
nachivpn <nachivpn@gmail.com>
2018-04-13 18:16:25 +0200

    adds test for ocp simplification

---
+++ test/bad/b19.erl
@@ -0,0 +1,12 @@
+-module(b19).
+-compile({parse_transform, etc}).
+
+-type either(A) :: {left,A} | {right, A}.
+-type dir(A) :: {left,A} | {right, A} | {fwd,A} | {bwd,A}.
+
+extract({left,A}) -> A;
+extract({right,A}) -> A.
+
+% un-unifiable return types
+foo12() -> extract({left,1.0});
+foo12() -> extract({left,true}).
